be4589c1f4094eac43e5e9be7ac655cdd0120309
QiuYi111 <2513868362@qq.com>
2024-07-05 11:19:58 +0800

    driver and list

--- LiteratureReview/auto/LiteratureReview.el
+++ LiteratureReview/auto/LiteratureReview.el
@@ -1,21 +1,26 @@
 (TeX-add-style-hook
  "LiteratureReview"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "table" "xcdraw")))
    (TeX-run-style-hooks
     "latex2e"
-    "paper"
-    "paper10"
+    "report"
+    "rep10"
     "ctex"
     "geometry"
     "graphicx"
     "amsmath"
     "cite"
     "subcaption"
-    "booktabs")
+    "booktabs"
+    "multirow"
+    "xcolor")
    (LaTeX-add-labels
     "sec:label"
     "subsec:label"
-    "fig:label")
+    "fig:label"
+    "tab:my-table")
    (LaTeX-add-bibliographies
     "references"))
  :latex)
